51948f39616e9208741dedcdb56fe1033d52d7d7
TalhaAhmed36 <41064991+TalhaAhmed36@users.noreply.github.com>
2022-02-13 16:47:37 +0500

    Add files via upload

---
+++ counter-app/main.js
@@ -0,0 +1,35 @@
+const count = document.querySelector(".count");
+
+const add = document.querySelector(".add");
+
+const sub = document.querySelector(".subtract");
+
+const resetNum = document.querySelector(".reset");
+
+add.addEventListener("click" , function(){
+    count.innerHTML++;
+    setColor();
+} )
+
+sub.addEventListener("click" , function(){
+    count.innerHTML--;
+    setColor();
+
+} )
+
+resetNum.addEventListener("click" , function(){
+    count.innerHTML = 0;
+    setColor();
+})
+
+    function setColor() {
+        if (count.innerHTML>0) {
+            count.style.color = "green" ; 
+        }
+        else if(count.innerHTML<0)  {
+            count.style.color = "red" ; 
+            }
+        else  {
+         count.style.color = "purple";
+            }
+    }
